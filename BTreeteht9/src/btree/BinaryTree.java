6f4f61a8fbd65296c7c06ddf4b470a633d4b6bed
Lappari <Joonas.Lapinlampi@gmail.com>
2019-12-01 17:05:48 +0200

    teht 11 jaoitteluhaku

--- BTreeteht9/src/btree/BinaryTree.java
+++ BTreeteht9/src/btree/BinaryTree.java
@@ -11,9 +11,10 @@ package btree;
  */
 public class BinaryTree {
 
+	private int korkeus = 0;
 	private Node root;
 	public static String found; // findWithPreOrder()-operaation apurakenne
-
+	
 	public BinaryTree(String rootValue) {
 		setRoot(new Node(rootValue));
 	}
@@ -22,10 +23,11 @@ public class BinaryTree {
 	 * public BinaryTree(String rootValue, BinaryTree left, BinaryTree right){ root
 	 * = new Node(rootValue, left, right); }
 	 */
-
+	
+	//binääri puun tulostus esijärjestyksessä
 	public void preOrder() {
 		if (getRoot() != null) {
-			System.out.println(getRoot().getData() + ',');
+			System.out.println(getRoot().getData() +" "+ korkeus +',');
 			if (getRoot().left() != null) // pääseeekö vasemmalle?
 				getRoot().left().preOrder();
 			if (getRoot().right() != null) // pääseekö oikealle?
@@ -33,24 +35,24 @@ public class BinaryTree {
 		}
 
 	}
-
+	
+	//lehden/solmun etsintä
 	public boolean findOrder(String haku) {
 		boolean bFound = false;
 		if (getRoot() != null) {
 			// System.out.println(root.getData()+',');
 			if (getRoot().getData().equals(haku)) {
-				found += "juuressa ";
+				found += korkeus+":juuressa";
 				bFound = true;
 			} else {
 
 				if (getRoot().left() != null && !bFound) {
-					System.out.println(getRoot().left().getRoot().getData());
-					found += "vasen ";
+					found += korkeus+ ":vasen ";
 					bFound = getRoot().left().findOrder(haku);// pääseeekö vasemmalle?
 				}
 
 				if (getRoot().right() != null && !bFound) { // pääseekö oikealle?
-					found += "oikea ";
+					found += korkeus+":oikea ";
 					bFound = getRoot().right().findOrder(haku);
 				}
 			}
@@ -59,22 +61,26 @@ public class BinaryTree {
 		return bFound;
 	}
 
-	// löydetty alipuu asetetaan staattiseen muuttujaan found
+	//asettaa automaattisestis uuden lehden oikeaan paikkaan stringi vertailua käyttäen
 	public void findWithPreOrder(BinaryTree newTree) {
 
 		if (getRoot() != null) {
-			// System.out.print(root.getData()+ ": muokkaatko tätä?");
+			//testi string
+			//System.out.println(stringCompare(newTree.getRoot().getData(), getRoot().getData())+" "+newTree.getRoot().getData()+" "+korkeus+" "+root.getData());
 			if (stringCompare(newTree.getRoot().getData(), getRoot().getData()) < 0) {
+				newTree.korkeus++;
 				if (getRoot().left() == null) {
 					setLeft(newTree);
-				} else if (getRoot().left() != null) {// pääseekö vasemmalle?
+				} else if (getRoot().left() != null && stringCompare(newTree.getRoot().getData(), getRoot().getData()) < 0) {// pääseekö vasemmalle?
 					getRoot().left().findWithPreOrder(newTree);
 				}
 
-			} else if (stringCompare(newTree.getRoot().getData(), getRoot().getData()) > 0) {
+			}
+			if (stringCompare(newTree.getRoot().getData(), getRoot().getData()) > 0) {
+				newTree.korkeus++;
 				if (getRoot().right() == null) {
 					setRight(newTree);
-				} else if (getRoot().right() != null) {// pääseekö oikealle?
+				} else if (getRoot().right() != null && stringCompare(newTree.getRoot().getData(), getRoot().getData()) > 0) {// pääseekö oikealle?
 					getRoot().right().findWithPreOrder(newTree);
 				}
 
@@ -106,17 +112,19 @@ public class BinaryTree {
 		int l1 = str1.length();
 		int l2 = str2.length();
 		int lmin = Math.min(l1, l2);
+		int str1_ch = 0;
+		int str2_ch = 0;
 
 		for (int i = 0; i < lmin; i++) {
-			int str1_ch = (int) str1.charAt(i);
-			int str2_ch = (int) str2.charAt(i);
-
-			if (str1_ch != str2_ch) {
-				return str1_ch - str2_ch;
-			}
+			str1_ch = (int) str1.charAt(i);
+			str2_ch = (int) str2.charAt(i);			
 		}
 
+		if (str1_ch != str2_ch) {				
+			return str1_ch - str2_ch;
+		}
 		if (l1 != l2) {
+			
 			return l1 - l2;
 		}
 
@@ -124,14 +132,12 @@ public class BinaryTree {
 			return 0;
 		}
 	}
-
+	
+	//etsitään poistettava solmu/lehti lähetetään se menulle ja poistetaan puusta
 	public BinaryTree findOrderDelete(String poisto) {
 		BinaryTree tFound = null;
 		if (getRoot() != null && tFound == null) {
-			/*
-			 * if (root.getData().equals(poisto)) { tFound = this; }
-			 */
-
+	
 			if (root.left() != null && root.left().root.getData().equals(poisto) && tFound == null) {
 				tFound = root.left();
 				root.setLeft(null);
@@ -160,4 +166,37 @@ public class BinaryTree {
 	public void setRoot(Node root) {
 		this.root = root;
 	}
+	
+	//etsintä jaoittelulla
+		public boolean searchWithPreOrder(String haku) {
+			boolean bFound = false;
+		
+			if (getRoot() != null) {
+				//testi string
+				//System.out.println(stringCompare(newTree.getRoot().getData(), getRoot().getData())+" "+newTree.getRoot().getData()+" "+korkeus+" "+root.getData());
+				if (getRoot().getData().equals(haku)) {
+					found += korkeus+":juuressa";
+					bFound = true;
+				} else {
+
+					//vasemmalle
+					if (stringCompare(haku, getRoot().getData()) < 0) {
+						if (getRoot().left() != null && !bFound) {
+							found += korkeus+ ":vasen ";
+							bFound = getRoot().left().findOrder(haku);// pääseeekö vasemmalle?
+						}
+					}
+					//oikealle
+					if (stringCompare(haku, getRoot().getData()) > 0) {
+						if (getRoot().right() != null && !bFound) { // pääseekö oikealle?
+							found += korkeus+":oikea ";
+							bFound = getRoot().right().findOrder(haku);
+						}
+					}
+				}
+
+			}
+			return bFound;
+
+		}
 }
--- BTreeteht9/src/btree/Menu.java
+++ BTreeteht9/src/btree/Menu.java
@@ -23,15 +23,21 @@ public class Menu {
 		char select, select1;
 		BinaryTree tree = null, upDated = null;
 		String data;
-
+		String haku;
+		
 		tree = new BinaryTree("juurisolmu");
+		tree.findWithPreOrder(new BinaryTree("lehti"));
 		tree.findWithPreOrder(new BinaryTree("lehti1"));
-		tree.findWithPreOrder(new BinaryTree("lehti12"));
-		tree.findWithPreOrder(new BinaryTree("lehti13"));
-		tree.findWithPreOrder(new BinaryTree("lehti14"));
+		tree.findWithPreOrder(new BinaryTree("lehti2"));
+		tree.findWithPreOrder(new BinaryTree("lehti3"));
 		tree.findWithPreOrder(new BinaryTree("l1"));
 		tree.findWithPreOrder(new BinaryTree("l12"));
 		tree.findWithPreOrder(new BinaryTree("l123"));
+		tree.findWithPreOrder(new BinaryTree("l1234"));
+		tree.findWithPreOrder(new BinaryTree("lehtipuuuuuuuu"));
+		tree.findWithPreOrder(new BinaryTree("lehtipuuuuuuuuu"));
+		tree.findWithPreOrder(new BinaryTree("lehtipuuuuuuuuuu"));
+		tree.findWithPreOrder(new BinaryTree("lehtipuuuuuuuuuuu"));
 		do {
 
 			System.out.println("\n\t\t\t1. Luo juurisolmu.");
@@ -39,7 +45,8 @@ public class Menu {
 			System.out.println("\t\t\t3. Käy puu läpi esijärjestyksessä.");
 			System.out.println("\t\t\t4. Etsi solmu.");
 			System.out.println("\t\t\t5. Poista solmu.");
-			System.out.println("\t\t\t6. lopetus ");
+			System.out.println("\t\t\t6. Jaoittelu haku.");
+			System.out.println("\t\t\t7. lopetus ");
 			System.out.print("\n\n"); // tehdään tyhjiä rivejä
 			select = Lue.merkki();
 			switch (select) {
@@ -64,7 +71,7 @@ public class Menu {
 				break;
 			case '4':
 				System.out.println("Anna haettavan solmun sisältö (merkkijono)");
-				String haku = Lue.rivi();
+				haku = Lue.rivi();
 				tree.setNotFound();
 				if (tree.findOrder(haku)) {
 					System.out.println(tree.getFound());
@@ -94,9 +101,20 @@ public class Menu {
 
 				break;
 			case '6':
+				System.out.println("Anna haettavan solmun sisältö (merkkijono)");
+				haku = Lue.rivi();
+				tree.setNotFound();
+				if (tree.searchWithPreOrder(haku)) {
+					System.out.println(tree.getFound());
+				} else {
+					System.out.println("Ei löydy.");
+					System.out.println(tree.getFound());
+				}
+				break;
+			case '7':
 				break;
 			}
-		} while (select != '6');
+		} while (select != '7');
 	}
 //printMenu loppuu ----------------------------------------------------------------
 }
